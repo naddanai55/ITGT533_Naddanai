8c9519a1434dc01817ad7c2e92333d9f627c6959
naddanai5 <naddanaiparod55@gmail.com>
2025-05-12 11:04:48 +0700

    fix: use func

--- Reversal_String/main.cpp
+++ Reversal_String/main.cpp
@@ -2,20 +2,24 @@
 
 using namespace std;
 
+void textSwap(char* ptr_g);
+
 int main()
 {
-    char g[] = "Hello";
-    int len = 0;
-    while (g[len] != '\0')
-    {
-        len++;
-    }
-    cout << "Original string: " << g << endl;
-    cout << "Reversed string: ";
+    char g[100] = ""; 
+    cout << "Original: ";
+    cin >> g; 
+    char* ptr_g = g;
+    cout << "Reversed: ";
+    textSwap(ptr_g);
+    return 0;
+}
+
+void textSwap(char* ptr_g)
+{
+    int len = strlen(ptr_g);
     for (int i = len - 1; i >= 0; i--)
     {
-        cout << g[i];
+        cout << *(ptr_g + i);
     }
-    cout << endl;
-    return 0;
 }
